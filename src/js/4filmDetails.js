3d1c9a5f65f0cd5eec9c3c3afe77ef06c282290a
Roman Svitelskyi <roman.svitelskyi@gmail.com>
2020-01-24 10:36:52 +0200

    change in details_page

---
+++ src/js/4filmDetails.js
@@ -0,0 +1,14 @@
+
+const BASE = "//api.themoviedb.org/3/movie/popular?api_key=4aa539255aa0c2506cf45806a15a8a0a&language=en-US&page=1"
+
+const wotched = document.querySelector(".btn-addWatch");
+const queue = document.querySelector(".btn-addQueue");
+const infoFilmsQueue = localStorage.getItem("filmsQueue");
+const infoFilmsWatched = localStorage.getItem("filmsWatched");
+console.log(infoFilmsQueue)
+
+window.addEventListener("load", monitorButtonStatusTextfunction);
+function monitorButtonStatusText() {
+}
+
+
