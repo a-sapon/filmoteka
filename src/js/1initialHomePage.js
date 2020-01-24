64729d19bce7a6d57906e8e56b11c14044671358
DatexG <datex.provide.life@gmail.com>
2020-01-24 10:38:15 +0200

    commit 2

--- src/js/1initialHomePage.js
+++ src/js/1initialHomePage.js
@@ -2,6 +2,9 @@
 
 const menuItem = document.querySelector('#menu-overlay')
 const API_KEY = 'e9f6322f77334e3f0406d6b8eabd79ce';
-const popularFilm = 'https://api.themoviedb.org/3/movie/550?api_key=e9f6322f77334e3f0406d6b8eabd79ce/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22'
+const BASE_URL = `https://api.themoviedb.org//discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc?api_key=e9f6322f77334e3f0406d6b8eabd79ce`
+const ACCSES_TOKEN = ``
 
-fetch(popularFilm)
+fetch(BASE_URL).then(res => res).then(data => console.log(data))
+
+console.log()
