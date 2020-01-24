d84618b72527547e838831085bf18ed37e2675a6
a-sapon <sapon.anya@gmail.com>
2020-01-24 15:17:56 +0200

    adding prev and next btns functionality

--- src/js/1initialHomePage.js
+++ src/js/1initialHomePage.js
@@ -8,6 +8,7 @@ const refs = {
   searchForm: document.querySelector('.search-form'),
   searchInput: document.querySelector('input[name="search"]'),
   filmsContainer: document.querySelector('.films_list'),
+  paginationPage: document.querySelector('.pagination_page'),
   btnNext: document.querySelector('.next'),
   btnPrev: document.querySelector('.prev')
 };
@@ -21,13 +22,10 @@ const filmsService = {
   resetPage() {
     this.page = 1;
   },
-  fetchFilms() {
-    fetch(
-      `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${this.value}&page=${this.page}&include_adult=true`,
-    )
+  fetchFilms(url) {
+    fetch(url)
       .then(response => response.json())
       .then(data => {
-        console.log(data.results);
         if(data.results.length !== 0) {
           data.results.map(film => {
             const markup = `
@@ -45,6 +43,7 @@ const filmsService = {
             refs.filmsContainer.insertAdjacentHTML('beforeend', markup);
           });
           this.incrementPage();
+          
         } else {
           const errorDiv = document.createElement('div');
           errorDiv.classList.add('error');
@@ -57,26 +56,76 @@ const filmsService = {
         }
       })
       .catch(e => console.log(e));
-  },
-  disablePaginationBtn() {
-    //
+      console.log(this.page);
   }
 };
 
 refs.searchForm.addEventListener('submit', searchFilms);
 refs.btnNext.addEventListener('click', loadNextPage);
+document.addEventListener('DOMContentLoaded', homePageFilm);
+refs.paginationPage.textContent = filmsService.page;
 
 function searchFilms(e) {
   e.preventDefault();
   filmsService.resetPage();
   refs.filmsContainer.innerHTML = '';
   filmsService.value = refs.searchInput.value;
-  filmsService.fetchFilms();
+  filmsService.fetchFilms(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${this.value}&page=${this.page}&include_adult=true`);
   refs.searchInput.value = '';
 }
 
 function loadNextPage() {
+  console.log('loadNextPage')
   refs.filmsContainer.innerHTML = '';
   filmsService.fetchFilms();
   refs.searchInput.value = '';
+  refs.paginationPage.textContent = filmsService.page;
+  refs.btnPrev.disabled = false;
 }
+
+
+
+
+
+
+// MAXIM:
+
+function homePageFilm() {
+  filmsService.fetchFilms(`${BASE_URL}/movie/popular?api_key=${API_KEY}&page=${filmsService.page}`);
+
+  // fetch(
+  //   `${BASE_URL}/movie/popular?api_key=${API_KEY}&page=1`,
+  // ).then(response => response.json()).then(data => {
+  //     data.results.map(film => {
+  //       const markup = `
+  //     <li class="films_list-item">
+  //       <a href="#">
+  //         <img 
+  //           src="https://image.tmdb.org/t/p/w500${film.backdrop_path}"
+  //           alt="${film.title} image"
+  //           class="films_list-item-image"
+  //         >
+  //         <h3 class="films_list-item-title">${film.title}</h3>
+  //       </a>
+  //     </li>
+  //     `;
+  //       refs.filmsContainer.insertAdjacentHTML('beforeend', markup);
+  //     })
+  //   });
+}
+
+function genreFilms() {
+  const genres = [];
+  fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}&page=1`)
+  .then(response => response.json())
+  .then(data => {
+    data.results.map(film => {
+      genres.push(film.genre_ids)
+    });
+  });
+  return genres;
+}
+
+// usages exmple:
+const genres = genreFilms();
+console.log(genres)
